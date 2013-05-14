b68781151c76d10735d94a79766bb2e1ef18a23c
Mark Gandolfo <mark@aussiev8.com.au>
2013-05-14 22:36:08 +1000

    cleaned up the http server code a little

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
-require 'socket'
 
 # Specify your gem's dependencies in httpserver.gemspec
 gemspec
--- lib/httpserver.rb
+++ lib/httpserver.rb
@@ -1,23 +1,7 @@
-require "httpserver/version"
+require 'socket'
+require "./lib/httpserver/mime"
+require "./lib/httpserver/version"
+require "./lib/httpserver/response"
+require "./lib/httpserver/webserver"
 
-module Httpserver
-	
-	webserver = TCPServer.new('127.0.0.1', 7125)
-	while (session = webserver.accept)
-	   session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
-	   request = session.gets
-	   trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '')
-	   filename = trimmedrequest.chomp
-	   if filename == ""
-	      filename = "index.html"
-	   end
-	   begin
-	      displayfile = File.open(filename, 'r')
-	      content = displayfile.read()
-	      session.print content
-	   rescue Errno::ENOENT
-	      session.print "File not found"
-	   end
-	   session.close
-	end
-end
+Httpserver::WebServer.new
